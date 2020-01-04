da6e4080e66498449015dc1cb4f7c1dde7954f9a
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-01-04 18:53:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,10 +26,10 @@ class ApplicationController < Sinatra::Base
     redirect '/articles/#{@article.id}'
   end
   
-  # get '/articles' do
-  #   @articles = Article.all
-  #   erb :show
-  # end
+  get '/articles' do
+    @articles = Article.all
+    erb :show
+  end
   
   # get '/articles/:id/edit' do
   #   @article = Article.find(params[:id])
