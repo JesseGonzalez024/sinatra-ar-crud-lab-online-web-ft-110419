f26c833a5951b2c6bfc2a25f1a99c51b1d2d1775
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-01-04 19:48:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,6 +43,7 @@ class ApplicationController < Sinatra::Base
   # end
   
   patch '/articles/:id' do
+    binding.pry
     article = Article.find(params[:id])
     article.update(params)
     redirect "/articles/#{article.id}"
