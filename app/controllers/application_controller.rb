06a858dd8c61de9333accac334bee3d9efb9c381
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-01-04 19:57:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,7 @@ class ApplicationController < Sinatra::Base
   
   patch '/articles/:id' do
     @article = Article.find_by_id(params[:id])
-    @article.title = params[:name]
+    @article.title = params[:title]
     binding.pry
     @article.content = params[:content]
     @article.save
